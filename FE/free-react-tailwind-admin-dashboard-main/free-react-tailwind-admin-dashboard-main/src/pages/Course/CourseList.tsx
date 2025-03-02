76f30ee7d7f9dcf7a90f4faeae760586f7558ad0
TruonggDX <dinhxuantruong2003coi@gmail.com>
2025-03-02 13:16:21 +0700

    search couse by condition

--- FE/free-react-tailwind-admin-dashboard-main/free-react-tailwind-admin-dashboard-main/src/pages/Course/CourseList.tsx
+++ FE/free-react-tailwind-admin-dashboard-main/free-react-tailwind-admin-dashboard-main/src/pages/Course/CourseList.tsx
@@ -1,24 +1,46 @@
 import Breadcrumb from '../../components/Breadcrumbs/Breadcrumb.tsx';
-import { Eye, Pencil, Trash2 } from 'lucide-react';
-import { deleteCourse, getCourses } from '../../service/CourseService.ts';
-import { useEffect, useState } from 'react';
+import { Building, CheckCircle, Eye, FileText, List, Pencil, RefreshCcw, Search, Tag, Trash2 } from 'lucide-react';
+import { deleteCourse, getCourseByCondition, getCourses } from '../../service/CourseService.ts';
+import  { useEffect, useState } from 'react';
 import { Course } from '../../types/Course.ts';
 import { useNavigate } from 'react-router-dom';
 import { confirmDelete } from '../../utils/swalUtils';
 import { formatCurrency } from '../../utils/Utils.ts';
+import { listCategories } from '../../service/CategoryService.ts';
+import { Category } from '../../types/Category.ts';
 const CourseList = () => {
   const [courses, setCourses] = useState<Course[]>([]);
   const [currentPage, setCurrentPage] = useState(0);
   const [itemsPerPage, setItemsPerPage] = useState(5);
   const [totalCertificate, setTotalCertificate] = useState(0);
-
+  const [categories,setCategories] = useState<Category[]>([]);
+  const [param,setParam] = useState({
+    category:'',
+    // status:'',
+    // code:'',
+    name:'',
+    // aim:''
+  })
 
   const navigate = useNavigate();
 
   useEffect(() => {
     void setItemsPerPage;
-    getAllCourse()
+    // getAllCourse()
+    getData()
+    listCategories(currentPage, itemsPerPage).then((response:any) =>{
+      setCategories(response.content)
+    })
+
   }, [currentPage,itemsPerPage]);
+
+  function getData(){
+    if ( param.category || param.name ){
+      dataSearch()
+    }else {
+      getAllCourse()
+    }
+  }
 
   function getAllCourse(){
     getCourses(currentPage, itemsPerPage).then((response:any) =>{
@@ -27,6 +49,7 @@ const CourseList = () => {
       setTotalCertificate(response.totalElements)
     }).catch((error: any) => console.error(error));
   }
+
 
 
   const totalPages = Math.ceil(totalCertificate / itemsPerPage);
@@ -60,57 +83,159 @@ const CourseList = () => {
       }).catch((error: any) => console.error(error));
     })
   }
+  const handleSearch = () =>{
+    dataSearch()
+  }
+  function dataSearch(){
+    getCourseByCondition(0,0,param).then((response:any) =>{
+      console.log('data search',response.content)
+    })
+  }
+
   return (
     <>
       <Breadcrumb pageName="Danh sách khóa học" />
+      <div className="flex flex-wrap items-center gap-4 mb-6 p-4 bg-white rounded-xl shadow-md">
+        <div className="relative flex-1 min-w-[250px]">
+          <FileText
+            className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500"
+            size={20}
+          />
+          <input
+            type="text"
+            name="certificateNumber"
+            // onChange={(e) => setParam({...param,code:e.target.value})}
+            className="w-full rounded-lg border border-gray-300 bg-white py-3 pl-10 pr-4 text-black outline-none transition focus:border-blue-500 focus:ring-2 focus:ring-blue-200"
+            placeholder="Mã khóa học"
+          />
+        </div>
+
+        <div className="relative flex-1 min-w-[350px]">
+          <Tag
+            className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500"
+            size={20}
+          />
+          <input
+            type="text"
+            name="certificateName"
+            className="w-full rounded-lg border border-gray-300 bg-white py-3 pl-10 pr-4 text-black outline-none transition focus:border-blue-500 focus:ring-2 focus:ring-blue-200"
+            placeholder="Tên khóa học"
+            onChange={(e) =>  setParam({ ...param, name: e.target.value })}
+          />
+        </div>
+
+        <div className="relative flex-1 min-w-[250px]">
+          <Building
+            className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500"
+            size={20}
+          />
+          <input
+            type="text"
+            name="issuingOrganization"
+            className="w-full rounded-lg border border-gray-300 bg-white py-3 pl-10 pr-4 text-black outline-none transition focus:border-blue-500 focus:ring-2 focus:ring-blue-200"
+            placeholder="Mục tiêu"
+            // onChange={(e) => setParam({...param,aim:e.target.value})}
+          />
+        </div>
+        <button className="flex items-center justify-center gap-2 bg-gray-500 text-white py-3 px-6 rounded-lg shadow-lg hover:bg-gray-700 transition-all text-lg font-semibold">
+          <RefreshCcw size={20} />
+          Làm mới
+        </button>
+        <div className="relative flex-1 min-w-[350px]">
+          <List
+            className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500"
+            size={20}
+          />
+          <select
+            onChange={(e) =>  setParam({ ...param, category: e.target.value })}
+            className="w-full rounded-lg border border-gray-300 bg-white py-3 pl-10 pr-4 text-black outline-none transition focus:border-blue-500 focus:ring-2 focus:ring-blue-200">
+            <option>Chọn danh mục</option>
+            {categories.map((categories) => (
+              <option key={categories.id} value={Number(categories.id)}>{categories.name}</option>
+            ))}
+          </select>
+        </div>
+
+        <div className="relative flex-1 min-w-[250px]">
+          <CheckCircle
+            className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500"
+            size={20}
+          />
+          <select
+            // onChange={(e) => setParam({...param,status: e.target.value})}
+            className="w-full rounded-lg border border-gray-300 bg-white py-3 pl-10 pr-4 text-black outline-none transition focus:border-blue-500 focus:ring-2 focus:ring-blue-200">
+            <option>Chọn trạng thái</option>
+            <option value="Đã duyệt">Đã Duyệt</option>
+            <option value="Chờ duyệt">Chờ Duyệt</option>
+          </select>
+        </div>
+
+        <button
+          onClick={handleSearch}
+          className="flex items-center justify-center gap-2 bg-blue-600 text-white py-3 px-6 rounded-lg shadow-lg hover:bg-blue-700 transition-all text-lg font-semibold">
+          <Search size={20} />
+          Tìm kiếm
+        </button>
+      </div>
       <div className="flex flex-col gap-10">
         <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
           <div className="max-w-full overflow-x-auto">
             <table className="w-full table-auto text-left">
               <thead>
-              <tr className="bg-gray-100 text-gray-700">
-                <th className="py-4 px-6 font-semibold">STT</th>
-                <th className="py-4 px-6 font-semibold">Mã khóa học</th>
-                <th className="py-4 px-6 font-semibold">Ảnh</th>
-                <th className="py-4 px-6 font-semibold">Tên khóa học</th>
-                <th className="py-4 px-6 font-semibold">Giá</th>
-                <th className="py-4 px-6 font-semibold">Trạng thái</th>
-                <th className="py-4 px-6 font-semibold">Thao tác</th>
-              </tr>
+                <tr className="bg-gray-100 text-gray-700">
+                  <th className="py-4 px-6 font-semibold">STT</th>
+                  <th className="py-4 px-6 font-semibold">Mã khóa học</th>
+                  <th className="py-4 px-6 font-semibold">Ảnh</th>
+                  <th className="py-4 px-6 font-semibold">Tên khóa học</th>
+                  <th className="py-4 px-6 font-semibold">Giá</th>
+                  <th className="py-4 px-6 font-semibold">Trạng thái</th>
+                  <th className="py-4 px-6 font-semibold">Thao tác</th>
+                </tr>
               </thead>
               <tbody>
-              {courses.map((course, index) => (
-                <tr key={course.id}>
-                  <td className="py-4 px-6">
-                    {index + 1 + currentPage * itemsPerPage}
-                  </td>
-                  <td className="py-4 px-6">{course.code}</td>
-                  <td className="py-4 px-6">
-                    <img
-                      src={course.imageUrl}
-                      style={{width:80,height:80,}}
-                      alt="Course Image 1"
-                      className="w-16 h-16 object-cover"
-                    />
-                  </td>
-                  <td className="py-4 px-6">{course.name}</td>
-                  <td className="py-4 px-6">{formatCurrency(course.price)} </td>
-                  <td className="py-4 px-6">{course.status}</td>
-                  <td className="py-4 px-6 flex gap-4" style={{marginTop:'15%'}}>
-                    <button className="text-blue-600 hover:text-blue-800 transition">
-                      <Eye onClick={() => handleShow(course.id)} size={20} />
-                    </button>
-                    <button className="text-yellow-600 hover:text-yellow-800 transition">
-                      <Pencil onClick={() => handleEdit(course.id)} size={20} />
-                    </button>
-                    <button
-                      onClick={() =>{handleRemove(course.id)}}
-                      className="text-red-600 hover:text-red-800 transition">
-                      <Trash2 size={20} />
-                    </button>
-                  </td>
-                </tr>
-              ))}
+                {courses.map((course, index) => (
+                  <tr key={course.id}>
+                    <td className="py-4 px-6">
+                      {index + 1 + currentPage * itemsPerPage}
+                    </td>
+                    <td className="py-4 px-6">{course.code}</td>
+                    <td className="py-4 px-6">
+                      <img
+                        src={course.imageUrl}
+                        style={{ width: 80, height: 80 }}
+                        alt="Course Image 1"
+                        className="w-16 h-16 object-cover"
+                      />
+                    </td>
+                    <td className="py-4 px-6">{course.name}</td>
+                    <td className="py-4 px-6">
+                      {formatCurrency(course.price)}{' '}
+                    </td>
+                    <td className="py-4 px-6">{course.status}</td>
+                    <td
+                      className="py-4 px-6 flex gap-4"
+                      style={{ marginTop: '15%' }}
+                    >
+                      <button className="text-blue-600 hover:text-blue-800 transition">
+                        <Eye onClick={() => handleShow(course.id)} size={20} />
+                      </button>
+                      <button className="text-yellow-600 hover:text-yellow-800 transition">
+                        <Pencil
+                          onClick={() => handleEdit(course.id)}
+                          size={20}
+                        />
+                      </button>
+                      <button
+                        onClick={() => {
+                          handleRemove(course.id);
+                        }}
+                        className="text-red-600 hover:text-red-800 transition"
+                      >
+                        <Trash2 size={20} />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
             <nav aria-label="Page navigation example">
--- FE/free-react-tailwind-admin-dashboard-main/free-react-tailwind-admin-dashboard-main/src/service/CourseService.ts
+++ FE/free-react-tailwind-admin-dashboard-main/free-react-tailwind-admin-dashboard-main/src/service/CourseService.ts
@@ -1,16 +1,19 @@
 import axios from 'axios';
-import {API_KEY,getToken} from '../config/Config.ts'
+import { API_KEY, getToken } from '../config/Config.ts';
 import { Course } from '../types/Course.ts';
 
 const api = `${API_KEY}/api/course`;
 
-export const getCourses = async (page:number, size:number): Promise<Course[]> => {
+export const getCourses = async (
+  page: number,
+  size: number,
+): Promise<Course[]> => {
   const token = getToken();
   try {
-    const response = await axios.get(`${api}/list`,{
+    const response = await axios.get(`${api}/list`, {
       params: { page, size },
-      headers: { 'Authorization': `Bearer ${token}` }
-    })
+      headers: { Authorization: `Bearer ${token}` },
+    });
     return response.data;
   } catch (err) {
     console.log(err);
@@ -18,39 +21,39 @@ export const getCourses = async (page:number, size:number): Promise<Course[]> =>
   }
 };
 
-export const findCouseById = async (id:number) => {
+export const findCouseById = async (id: number) => {
   const token = getToken();
   try {
     const response = await axios.get(`${api}/findById/${id}`, {
-      headers: { 'Authorization': `Bearer ${token}` }
-    })
+      headers: { Authorization: `Bearer ${token}` },
+    });
     return response.data;
-  }catch (error) {
+  } catch (error) {
     console.error(error);
     throw error;
   }
-}
-export const deleteCourse = async (id:number) => {
+};
+export const deleteCourse = async (id: number) => {
   const token = getToken();
   try {
     const response = await axios.delete(`${api}/delete/${id}`, {
-      headers: { 'Authorization': `Bearer ${token}` }
-    })
+      headers: { Authorization: `Bearer ${token}` },
+    });
     return response.data;
-  }catch (error) {
+  } catch (error) {
     console.error(error);
     throw error;
   }
-}
+};
 export const createCourse = async (formData: FormData) => {
   const token = getToken();
 
   try {
     const response = await axios.post(`${api}`, formData, {
       headers: {
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': 'multipart/form-data'
-      }
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'multipart/form-data',
+      },
     });
 
     return response.data;
@@ -59,18 +62,46 @@ export const createCourse = async (formData: FormData) => {
     throw error;
   }
 };
-export const updateCourseById = async (id:number,formData:FormData) =>{
+export const updateCourseById = async (id: number, formData: FormData) => {
   const token = getToken();
   try {
     const response = await axios.put(`${api}/update/${id}`, formData, {
       headers: {
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': 'multipart/form-data'
-      }
-    })
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'multipart/form-data',
+      },
+    });
     return response.data;
-  }catch (error) {
+  } catch (error) {
     console.error(error);
     throw error;
   }
-}
+};
+export const getCourseByCondition = async (page: number, size: number, filter: any) => {
+  const token = getToken();
+  if (!token) {
+    throw new Error("Unauthorized: No token found");
+  }
+
+  try {
+    const params: any = {
+      page,
+      size,
+      ...(filter.category && { category: filter.category }),
+      ...(filter.status && { status: filter.status }),
+      ...(filter.code && { code: filter.code }),
+      ...(filter.name && { name: encodeURIComponent(filter.name) }),
+      ...(filter.aim && { aim: filter.aim }),
+    };
+
+    const response = await axios.get(`${api}/findByCondition`, {
+      headers: { Authorization: `Bearer ${token}` },
+      params:params,
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching courses:", error);
+    throw error;
+  }
+};
